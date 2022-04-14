fc2fdab48d8a2007dbca933c9e9dbd0811e729a0
dbk1109 <dabin1004i@naver.com>
2022-04-14 23:11:49 +0900

    Feat: update header menu text with side badges. Style: update header position fixed and badges will floating with header.

--- js/main.js
+++ js/main.js
@@ -8,9 +8,30 @@ searchEl.addEventListener('click', function () {
 searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
+});
 
 searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  //원래는 window.screenY를 사용했지만 듀얼모니터의 경우때문에 pageYOffset를 사용함
+  //_.throttle(함수, 시간)
+  //gsap.to(요소, 시간, 옵션);
+  if (window.pageYOffset > 500) {
+    //badgeEl.style.display = 'none';
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    //badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
